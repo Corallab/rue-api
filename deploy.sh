12a8a417af26b275e7b89cf2f3b2cd1b6a1341f9
Mohammed Musthafa <musthafa.m1996@gmail.com>
2024-12-28 03:39:05 +0530

    fix: deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Pull the latest changes from the repository
-cd /home/appuser/rue-api/backend
+cd /home/appuser/rue-api
 git pull origin main
 
 # Activate the virtual environment
-source /home/appuser/rue-api/backend/venv/bin/activate
+source /home/appuser/rue-api/venv/bin/activate
 
 # Install any new dependencies
 pip install -r requirements.txt
