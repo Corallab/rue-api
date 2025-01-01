a800d14518a1ff2eaa17810be07befe15e28ebf7
Mohammed Musthafa <musthafa.m1996@gmail.com>
2025-01-01 14:33:03 +0530

    chore: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Pull the latest changes from the repository
-cd /home/appuser/rue-api
+cd /home/appuser/rue-api-old
 git pull origin main
 
 # Activate the virtual environment
-source /home/appuser/rue-api/venv/bin/activate
+source /home/appuser/rue-api-old/venv/bin/activate
 
 # Install any new dependencies
 pip install -r requirements.txt
 
 # Restart the server (e.g., Gunicorn, Nginx, etc.)
-sudo systemctl restart rue-api
+sudo systemctl restart rue-api-old
